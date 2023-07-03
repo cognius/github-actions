1a269d2f63e5a02a2eb66e20539de3b8ff01058d
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-07-03 11:48:23 +0700

    feat(notify-deploy): refactor code and use same variable name as other

--- actions/notify-deploy/notify-deploy.sh
+++ actions/notify-deploy/notify-deploy.sh
@@ -1,64 +1,112 @@
 #!/usr/bin/env bash
 
-type="${NOTIFY_TYPE:?}"
-step="${NOTIFY_STEP:?}"
-enabled="${NOTIFY_ENABLED:-true}"
-component="${COMPONENT:?}"
-env="${ENVIRONMENT:?}"
-webhook="${SLACK_WEBHOOK:?}"
+_mode="${NOTIFY_MODE:-${RELEASE_MODE:?}}"
+_status="${NOTIFY_STATUS:-${NOTIFY_TYPE:?}}"
+_step="${NOTIFY_STEP:-}"
 
-mode="${RELEASE_MODE:?}"
-version="${RELEASE_VERSION:?}"
+_app_name="${APP_NAME:-${COMPONENT:?}}"
+_app_version="${APP_VERSION:-${RELEASE_VERSION:-}}"
+_environment="${ENVIRONMENT:-}"
 
-git_repo="${GIT_REPO:?}"
-git_ref="${GIT_REF:?}"
-git_author="${GIT_AUTHOR:?}"
-git_commit="${GIT_COMMIT:?}"
-gh_id="${GITHUB_ACTION_ID:?}"
+_webhook="${SLACK_WEBHOOK:?}"
 
-gh_commit_url="https://github.com/$git_repo/commit/$git_commit"
-gh_action_url="https://github.com/$git_repo/actions/runs/$gh_id"
+## Auto-assign by GitHub Action
+_git_repo="${GIT_REPO:?}"
+_git_ref="${GIT_REF:?}"
+_git_author="${GIT_AUTHOR:?}"
+_git_commit="${GIT_COMMIT:?}"
+_gh_id="${GITHUB_ACTION_ID:?}"
 
-gh_commit_link="<$gh_commit_url|$git_ref>"
-gh_action_link="<$gh_action_url|$gh_id>"
+main() {
+  local commit_url="https://github.com/$_git_repo/commit/$_git_commit"
+  local commit_md="<$commit_url|$_git_ref>"
 
-if test -z "$enabled" ||
-  [[ "$enabled" == false ]]; then
-  printf "disable notification\n"
-  exit 0
-fi
+  local action_url="https://github.com/$_git_repo/actions/runs/$_gh_id"
+  local action_md="<$action_url|$_gh_id>"
 
-message=""
-case "$type" in
-start)
-  [[ "$env" == "production" ]] && [[ "$git_ref" != "main" ]] &&
-    message+="<!here> "
-  message+="*$git_author* starting $mode \`${component}[$version]\`"
-  message+=" from $gh_commit_link"
-  message+=" to *$env*"
-  message+=" ($gh_action_link)"
-  ;;
-error | failure)
-  message+="<!here> Finished *$step* with \`error\`"
-  message+=", please check $gh_action_link"
-  ;;
-success)
-  message+="Finished *$step* successfully ($gh_action_link):
-  "
-  message+="*Component name*: $component
-  "
-  message+="*Environment*: $env
-  "
-  message+="*Version*: $version
-  "
-  message+="*Reference*: $gh_commit_link"
-  ;;
-esac
+  local args=($'\n' "$commit_md" "$action_md")
+  case "$_status" in
+  start | starting) send __start_message "${args[@]}" ;;
+  error | fail | failure) send __error_message "${args[@]}" ;;
+  success) send __success_message "${args[@]}" ;;
+  esac
 
-printf "sending message: %s\n" "$message"
-if test -n "$message"; then
-  curl -X POST \
-    -H 'Content-type: application/json' \
-    --data "{\"text\":\"$message\"}" \
-    "$webhook"
-fi
+  ## Never failed to send notification
+  return 0
+}
+
+__start_message() {
+  local newline="$1" commit="$2" action="$3"
+  local message=()
+
+  [[ "$_environment" == "production" ]] && [[ "$_git_ref" != "main" ]] &&
+    message+=("<!here>")
+  message+=("*$_git_author*" "starting" "$_mode")
+  test -n "$_app_version" &&
+    message+=("\`${_app_name}[${_app_version}]\`")
+  test -z "$_app_version" &&
+    message+=("\`${_app_name}\`")
+  message+=("from $commit")
+  test -n "$_environment" && message+=("to *$_environment*")
+  message+=("(${action})")
+
+  printf "%s" "${message[*]}"
+}
+
+__error_message() {
+  local newline="$1" commit="$2" action="$3"
+  local message=()
+
+  message+=("<!here>")
+  message+=("Finished *${_mode}*")
+  test -n "$_step" && message+=("with \`error\` at *$_step* step,")
+  test -z "$_step" && message+=("with \`error\`,")
+  message+=("please check $action")
+
+  printf "%s" "${message[*]}"
+}
+
+__success_message() {
+  local newline="$1" commit="$2" action="$3"
+  local message=()
+
+  message+=("Finished *$_mode* successfully $action" "$newline")
+  message+=("- *Application name*: $_app_name" "$newline")
+  test -n "$_environment" &&
+    message+=("- *Environment*: $_environment" "$newline")
+  test -n "$_app_version" &&
+    message+=("- *Application version*: \`$_app_version\`" "$newline")
+  message+=("- *Reference*: $commit" "$newline")
+
+  printf "%s" "${message[*]}"
+}
+
+send() {
+  local enabled="${NOTIFY_ENABLED:-true}" msg_fn="$1"
+  shift
+
+  local message
+  message="$("$msg_fn" "$@")"
+
+  printf "[INFO] preparing message\n\`\`\`\n%s\n\`\`\`\n\n" "$message"
+  if [[ "$enabled" == true ]]; then
+    if test -n "$message"; then
+      printf "[INFO] sending notification\n"
+      curl \
+        --silent --show-error --location \
+        --request POST --header 'Content-type: application/json' \
+        --data "{\"text\":\"$message\"}" \
+        "$_webhook"
+    fi
+  else
+    echo "[INFO] stopped notification (\$NOTIFY_ENABLED set to false)"
+    return 0
+  fi
+}
+
+main || exit 1
+
+unset _mode _step _status
+unset _app_name _app_version _environment
+unset _webhook
+unset _git_repo _git_ref _git_author _git_commit _gh_id
