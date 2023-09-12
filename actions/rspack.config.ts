e0d7a98a7d811b41192c1cb9eb1975fcd0e1df9e
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-09-12 22:23:35 +0700

    perf(actions): initiate actions with lint, tests and build

--- actions/rspack.config.ts
+++ actions/rspack.config.ts
@@ -1,89 +1,107 @@
-import type { Configuration } from "@rspack/cli";
-import { resolve, basename } from "node:path";
+import type { Configuration } from "@rspack/cli"
+import { resolve } from "node:path"
 
-type Callback<Conf, NewConf> = (config: Conf, ...args: any) => NewConf;
-type Merge<Object, Key extends string, Value> = Object & { [name in Key]: Value };
-type OmitFirst<T extends any[]> = T extends [any, ...infer R] ? R : never;
+type Callback<Conf, NewConf> = (config: Conf, ...args: any) => NewConf
+type Merge<BaseObject, Key extends string, Value> = BaseObject & {
+  [name in Key]: Value
+}
+type OmitFirst<T extends any[]> = T extends [any, ...infer R] ? R : never
 
 class ConfigWrapper {
-  constructor(private base: Configuration) {}
+  constructor(private readonly base: Configuration) {}
 
   setEntry(input: Exclude<Configuration["entry"], undefined>): this {
-    this.base.entry = input;
-    return this;
+    this.base.entry = input
+    return this
   }
 
-  addEntry(input: Exclude<Configuration["entry"], string | string[] | undefined>): this {
-    const entry = typeof this.base.entry === "object" ? this.base.entry ?? {} : {};
+  addEntry(
+    input: Exclude<Configuration["entry"], string | string[] | undefined>
+  ): this {
+    const entry =
+      typeof this.base.entry === "object" ? this.base.entry ?? {} : {}
     this.base.entry = {
       ...entry,
       ...input,
-    };
-    return this;
+    }
+    return this
   }
 
-  addEntryByName(name: string, input: Exclude<Configuration["entry"], string | string[] | undefined>[string]): this {
-    const entry = typeof this.base.entry === "object" ? this.base.entry ?? {} : {};
+  addEntryByName(
+    name: string,
+    input: Exclude<
+      Configuration["entry"],
+      string | string[] | undefined
+    >[string]
+  ): this {
+    const entry =
+      typeof this.base.entry === "object" ? this.base.entry ?? {} : {}
     this.base.entry = {
       ...entry,
       [name]: input,
-    };
-    return this;
+    }
+    return this
   }
 
   copy(name: string, basepath: string, ...paths: string[]): this {
-    this.base.builtins = this.base.builtins ?? {};
-    this.base.builtins.copy = this.base.builtins?.copy ?? { patterns: [] };
+    this.base.builtins = this.base.builtins ?? {}
+    this.base.builtins.copy = this.base.builtins?.copy ?? { patterns: [] }
 
-    const patterns = this.base.builtins?.copy?.patterns ?? [];
+    const patterns = this.base.builtins?.copy?.patterns ?? []
     patterns.push({
       from: resolve(basepath, ...paths),
       to: resolve(this.base.output?.path ?? "", name, ...paths),
-    });
+    })
 
-    this.base.builtins.copy.patterns = patterns;
-    return this;
+    this.base.builtins.copy.patterns = patterns
+    return this
   }
 
-  get config() {
-    return this.base;
+  get config(): Configuration {
+    return this.base
   }
 }
 
-class Config<Conf, Callbacks extends Record<string, Callback<unknown, unknown>> = {}> {
-  static builder<Conf extends Configuration>(base: Conf) {
-    return new Config(new ConfigWrapper(base));
+class Config<
+  Conf,
+  Callbacks extends Record<
+    string,
+    Callback<unknown, unknown>
+  > = NonNullable<unknown>,
+> {
+  static builder<Conf extends Configuration>(base: Conf): Config<Conf> {
+    return new Config(new ConfigWrapper(base))
   }
 
-  private callbacks: Callbacks;
-  private constructor(private base: ConfigWrapper) {
-    this.callbacks = {} as Callbacks;
+  private callbacks: Record<string, Callback<unknown, unknown>>
+  private constructor(private readonly base: ConfigWrapper) {
+    this.callbacks = {}
   }
 
-  setCallback<Name extends string, Function extends Callback<ConfigWrapper, ConfigWrapper>>(
-    name: Name,
-    callback: Function
-  ): Config<Conf, Merge<Callbacks, Name, Function>> {
-    this.callbacks[name] = callback as unknown as Callbacks[Name];
-    return this as unknown as Config<Conf, Merge<Callbacks, Name, Function>>;
+  setCallback<
+    Name extends string,
+    CB extends Callback<ConfigWrapper, ConfigWrapper>,
+  >(name: Name, callback: CB): Config<Conf, Merge<Callbacks, Name, CB>> {
+    this.callbacks[name] = callback as unknown as Callbacks[Name]
+    return this as unknown as Config<Conf, Merge<Callbacks, Name, CB>>
   }
 
   useCallback<Name extends keyof Callbacks>(
     name: Name,
     ...args: OmitFirst<Parameters<Callbacks[Name]>>
   ): Config<ReturnType<Callbacks[Name]>, Callbacks> {
-    this.callbacks[name](this.base, ...args) as ConfigWrapper;
-    return this as unknown as Config<ReturnType<Callbacks[Name]>, Callbacks>;
+    this.callbacks[name as string](this.base, ...args) as ConfigWrapper
+    return this as unknown as Config<ReturnType<Callbacks[Name]>, Callbacks>
   }
 
-  build() {
-    return this.base.config;
+  build(): Conf {
+    return this.base.config as Conf
   }
 }
 
 const relative = (...paths: string[]): string => {
-  return resolve(__dirname, ...paths);
-};
+  return resolve(__dirname, ...paths)
+}
 
 const builder = Config.builder({
   target: "node",
@@ -95,8 +113,11 @@ const builder = Config.builder({
     encoding: "encoding",
   },
 }).setCallback("module", (config, name: string) => {
-  const basepath = relative("src", name);
-  return config.addEntryByName(name, basepath).copy(name, basepath, "README.md").copy(name, basepath, "action.yaml");
-});
+  const basepath = relative("src", name)
+  return config
+    .addEntryByName(name, basepath)
+    .copy(name, basepath, "README.md")
+    .copy(name, basepath, "action.yaml")
+})
 
-export default builder.useCallback("module", "greeting").build();
+export default builder.useCallback("module", "greeting").build()
--- actions/src/greeting/index.ts
+++ actions/src/greeting/index.ts
@@ -1,16 +1,3 @@
-import { getInput, setFailed, setOutput } from "@actions/core";
-import { context } from "@actions/github";
+import { run } from "./app"
 
-try {
-  const nameToGreet = getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-
-  const time = new Date().toTimeString();
-  setOutput("time", time);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  setFailed((error as Error).message);
-}
+void run()
