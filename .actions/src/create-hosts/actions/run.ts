ced23b1e668f5c15d4998c1d97d4863f0c3b218a
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2023-09-18 10:02:15 +0700

    fix(create-hosts): try again and exec version instead

--- .actions/src/create-hosts/actions/run.ts
+++ .actions/src/create-hosts/actions/run.ts
@@ -1,7 +1,8 @@
 import type { Runner } from "@utils/actions"
 import type { Config } from "../app/types"
 
-import { appendFile } from "fs/promises"
+// import { appendFile } from "fs/promises"
+import { exec } from "@utils/executors"
 
 const action: Runner<Config> = async ({ ip, hosts, tableFile }) => {
   const lookupTable = hosts.reduce((table, host) => {
@@ -9,7 +10,11 @@ const action: Runner<Config> = async ({ ip, hosts, tableFile }) => {
     return table + "\n" + row
   }, "")
 
-  await appendFile(tableFile, lookupTable, { encoding: "utf8" })
+  // await exec("sudo", "chown", )
+  await exec("sudo", "printf", lookupTable, ">", tableFile)
+  // await appendFile(tableFile, lookupTable, {
+  //   encoding: "utf8",
+  // })
 }
 
 export default action
